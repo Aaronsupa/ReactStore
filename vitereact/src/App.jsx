6f44fdea9f2b41b5a65d9067ccead8d28be8a21a
Aaronsupa <awjohnson2023@gmail.com>
2023-08-17 19:38:52 -0500

    Layout Updates

--- vitereact/src/App.jsx
+++ vitereact/src/App.jsx
@@ -1,15 +1,81 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Tech, setTech] = useState(
+    [
+      {
+        id: 1,
+        Name: "Computer"
+      },
+      {
+        id: 2,
+        Name: "Laptop"
+      },
+      {
+        id: 1,
+        Name: "Keyboard"
+      },
+      {
+        id: 2,
+        Name: "Mouse"
+      },
+      {
+        id: 1,
+        Name: "Charger"
+      },
+      {
+        id: 2,
+        Name: "Headphones"
+      },
+      {
+        id: 1,
+        Name: "Extention Cord"
+      },
+      {
+        id: 2,
+        Name: "Camera"
+      },
+    ]
+  )
 
   return (
-    <div>
-      <p>Hello World</p>
-    </div>
+    <div className = "Container">
+      <div className = "Top">
+        <div>
+          <h1>Contact</h1>
+        </div>
+        <div>
+          <h1>BuyBuyTech</h1>
+        </div>
+        <div>
+          <h1>About Us</h1>
+        </div>
+        </div>
+        <div className = "Middle">
+          <div className = "Products">
+              <ul>
+                {Tech.map((t) => 
+                  <div key = {t.id}>
+                    <div className = "Prodtop">
+                    </div>
+                    <div className = "Prodbottom">
+                      <h3>
+                        {t.Name}
+                      </h3>
+                    </div>
+                  </div>
+                )}
+              </ul>
+          </div>
+        </div>
+        <div className = "Footer">
+          <div>
+            Github Link
+          </div>
+        </div>
+      </div>
+
   )
 }
 
