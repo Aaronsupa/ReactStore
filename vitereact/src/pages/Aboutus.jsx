c4cff0f1752d71d66cd41aafb3c931afe7da2e96
Aaronsupa <awjohnson2023@gmail.com>
2023-08-21 16:16:53 -0500

    Aboutus updated

--- vitereact/src/pages/Aboutus.jsx
+++ vitereact/src/pages/Aboutus.jsx
@@ -1,10 +1,41 @@
+import { useState, useEffect } from 'react';
+import '../App.css';
+import '../index.css';
+import './Contact'
+import { Link } from "react-router-dom";
+
 function Aboutus() {
 
     return(
-        <h1>
-            About Us!
-        </h1>
-    )
+            <div className = "Container" id = "who">
+              <div className = "Top">
+                <div>
+                  <Link to = "./Contact" className='Link'><h1>Contact</h1></Link>
+                </div>
+                <div>
+                  <h1>BuyBuyTech</h1>
+                </div>
+                <div>
+                  <Link to = "./Aboutus" className='Link'><h1>About Us</h1></Link>
+                </div>
+                </div>
+                <div className = "Middle">
+                    <div className='MiddleAboutus'>
+                        <h3>v yi yugvyu hukvuo yugvyuo ygu o yug oi yug b uvk
+                             kyuk yuk yu uyk yukg yukgukgku yukg
+                             dwada dwasda dwads dwadwas dwad sadwa
+                             dwasadw dwasadw dwas dwadasa dwasdwad dwasdad
+                             dwasdw dawdaw </h3>
+                    </div>
+                </div>
+                <div className = "Footer">
+                  <div>
+                    Github Link
+                  </div>
+                </div>
+              </div>
+        
+          )
 }
 
 export default Aboutus;
